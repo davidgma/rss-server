aa70e32bcbac7c4cd9d705a8ba4cae0e23d9d01b
David Ma <dmlh02@gmail.com>
2021-02-10 23:36:06 +0000

    started refactor for other feeds

--- src/control.ts
+++ src/control.ts
@@ -39,9 +39,24 @@ export class Controller {
 
     public async updateAll() {
         await this.createTableIfNotExist();
-        let showName = "una-mas-uno-14330";
-        let episodes = await this.#parser.getEpisodeLinks(showName, 10);
+        await this.updateFeed("una-mas-uno-14330",
+        "Una Más Uno ", "unamasuno", 10);
 
+        // writeFile(homedir + "/local/dev/rss-server/data/unamasuno2.rss", 
+        // this.#feed.rss2(), (err) => {
+        //     if (err) {
+        //         console.log("Error writing rss file: " + err.message);
+        //     }
+        // });
+
+        
+    }
+
+    private async updateFeed(showName: string, title: string, 
+        fileName: string, count: number) {
+        //let showName = "una-mas-uno-14330";
+        let episodes = await this.#parser.getEpisodeLinks(showName, count);
+            //console.log("here");
         for (let episode of episodes) {
             episode.downloadLink = await this.#parser.getDownloadLink(episode.episodeLink);
             console.log(JSON.stringify(episode));
@@ -50,56 +65,19 @@ export class Controller {
             let day: number = Number.parseInt(episode.episodeDate.substring(0,2));
             let ed = new Date(year, month, day);
             this.#feed.addItem({
-              title: "Una Más Uno " + episode.episodeDate,
+              title: title + episode.episodeDate,
               link: episode.downloadLink,
               date: ed  
             });
         }
 
 
-        writeFile(homedir + "/local/dev/rss-server/data/unamasuno.rss", 
+        writeFile(homedir + "/local/dev/rss-server/data/" + fileName + ".rss", 
         this.#feed.rss2(), (err) => {
             if (err) {
                 console.log("Error writing rss file: " + err.message);
             }
         });
-
-        writeFile(homedir + "/local/dev/rss-server/data/unamasuno2.rss", 
-        this.#feed.rss2(), (err) => {
-            if (err) {
-                console.log("Error writing rss file: " + err.message);
-            }
-        });
-
-        // writeFile(homedir + "/local/dev/rss-server/data/unamasuno.atom", this.#feed.atom1(), (err) => {
-        //     if (err) {
-        //         console.log("Error writing atom file: " + err.message);
-        //     }
-        // });
-       
-        // writeFile(homedir + "/local/dev/rss-server/data/unamasuno.json", this.#feed.json1(), (err) => {
-        //     if (err) {
-        //         console.log("Error writing json file: " + err.message);
-        //     }
-        // });
-
-        // let episode = episodes[0];
-        // await this.#parser.downloadMp3(episode.downloadLink, "./data/" 
-        // + showName + "_" + episode.episodeDate.replace("/", "-").replace("/", "-") + ".mp3");
-
-
-        // let sql = 'select symbol from t_prices;'
-        // let result = await this.#dbs.sendSql(sql);
-        // if (result != null) {
-        //     let symbols = result.split('\n');
-        //     for (let symbol of symbols) {
-        //         if (symbol.length != 0) {
-        //             await this.waitRandomTime(10);
-        //             console.log("Updating symbol:  " + symbol);
-        //             await this.update(symbol);
-        //         }
-        //     }
-        // }
     }
 
     private setUpFeed(): GoogleFeed {
@@ -115,10 +93,10 @@ export class Controller {
             copyright: "Canarias Radio RTVC",
             // updated: new Date(2013, 6, 14), // optional, default = today
             // generator: "awesome", // optional, default = 'Feed for Node.js'
-            feedLinks: {
-                json: "http://rss.davidgma.com/unamasuno.json",
-                atom: "http://rss.davidgma.com/unamasuno.atom"
-            },
+            // feedLinks: {
+            //     json: "http://rss.davidgma.com/unamasuno.json",
+            //     atom: "http://rss.davidgma.com/unamasuno.atom"
+            // },
             // author: {
             //     name: "John Doe",
             //     email: "johndoe@example.com",
