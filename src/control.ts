887bd2b3af8eb26ef4d86780829b34c46ffcd150
David Ma <dmlh02@gmail.com>
2021-07-06 18:36:27 +0100

    Radio Uruguay feed

--- src/control.ts
+++ src/control.ts
@@ -1,61 +1,16 @@
 // Program for updating the rss file
 
-//import { DatabaseService } from './db-service';
-import { GoogleFeed } from './google-feed';
-import { TardeFeed } from './tarde-feed';
-import { UnaMasUnoFeed } from './una-feed';
+import { Feed } from "feed";
+import {RadioUruguayFeed } from './radiouruguay-feed';
 
 export class Controller {
-    //#dbs = new DatabaseService("files.db");    
-    #feeds: GoogleFeed[] = new Array<GoogleFeed>();
-
-    constructor() { 
-        this.#feeds.push(new UnaMasUnoFeed());
-        this.#feeds.push(new TardeFeed());
-    }
-
-    // public async list() {
-
-    //     await this.createTableIfNotExist();
-    //     let sql = 'select * from t_prices;'
-    //     let result = await this.#dbs.sendSql(sql);
-    //     if (result != null) {
-    //         console.log(result);
-    //     }
-    // }
-
-    // private async createTableIfNotExist() {
-    //     let result: string;
-    //     let sql = 'create table if not exists t_files (filename text primary key, postdate datetime,'
-    //         + 'description text);'
-    //     result = await this.#dbs.sendSql(sql);
-    //     if (result != null) {
-    //         console.log(result);
-    //     }
-    // }
 
     public async updateAll() {
 
-        //await this.createTableIfNotExist();
+        let radioUruguayFeed = new RadioUruguayFeed();
+        await radioUruguayFeed.updateFeed();
 
-        for (let feed of this.#feeds) {
-            await feed.updateFeed();
-        }
-
-        // writeFile(homedir + "/local/dev/rss-server/data/unamasuno2.rss", 
-        // this.#feed.rss2(), (err) => {
-        //     if (err) {
-        //         console.log("Error writing rss file: " + err.message);
-        //     }
-        // });
-
-        
     }
-
-    
-
-    
-
 
 } // end of class controller
 
---
+++ src/radiouruguay-feed.ts
@@ -0,0 +1,57 @@
+import { Feed } from "feed";
+import { Parser } from './radiouruguay-parser';
+import { writeFile } from 'fs';
+import { homedir } from 'os';
+
+export class RadioUruguayFeed {
+
+    private feed: Feed;
+    private parser: Parser = new Parser();
+    private nameCode = "radiouruguay";
+    private title = "Radio Uruguay";
+
+    public constructor() {
+        this.feed = new Feed({
+            title: this.title,
+            description: "All mp3 files from Radio Uruguay's Directory Listings.",
+            id: "http://rss.davidgma.com/" + this.nameCode + ".rss",
+            link: "http://rss.davidgma.com/" + this.nameCode + ".rss",
+            language: "es", 
+            // optional, used only in RSS 2.0, possible values: 
+            // http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+            image: "http://rss.davidgma.com/" + this.nameCode + ".png",
+            // favicon: "http://example.com/favicon.ico",
+            copyright: this.title
+        });
+    }
+    
+    public async updateFeed() {
+        let episodes = await this.parser.getEpisodeLinks();
+        for (let episode of episodes) {
+            console.log(JSON.stringify(episode));
+            let year: number = Number.parseInt(episode.episodeDate.substring(0,4));
+            let month: number = Number.parseInt(episode.episodeDate.substring(5,7)) - 1;
+            let day: number = Number.parseInt(episode.episodeDate.substring(8,10));
+            let hour = Number.parseInt(episode.episodeDate.substring(11,13));
+            let minute = Number.parseInt(episode.episodeDate.substring(14,16));
+            let ed = new Date(year, month, day, hour, minute);
+            let title = episode.episodeLink.substring(0, episode.episodeLink.length - 4);
+            console.log(title);
+            this.feed.addItem({
+              title: title,
+              link: episode.downloadLink,
+              date: ed  
+            });
+        }
+
+
+        writeFile(homedir + "/local/dev/rss-server/data/" + this.nameCode + ".rss", 
+        this.feed.rss2(), (err) => {
+            if (err) {
+                console.log("Error writing rss file: " + err.message);
+            }
+        });
+    }
+
+    
+}
